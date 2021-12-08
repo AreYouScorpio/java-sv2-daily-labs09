700a3a9b201f80121b9579f339b7f14c4c1d6150
AreYouScorpio <akos.schweger@gmail.com>
2021-12-08 23:17:16 +0100

    init wk9 string task done

--- src/main/java/day02/StringLists.java
+++ src/main/java/day02/StringLists.java
@@ -5,23 +5,24 @@ import java.util.List;
 
 public class StringLists {
 
+
     public List<String> shortestWords(List<String> words){
         int max=1000;
         List<String> result = new ArrayList<>();
         for (String actual : words) {
             if (actual.length()<max) {max=actual.length();}
-            result.clear();
         }
         for (String actual : words) {
             if (actual.length()==max) result.add(actual);}
         return result;
-        }
 
+
+        /*
     public static void main(String[] args) {
         StringLists stringLists = new StringLists();
         stringLists.shortestWords(List.of("aaa","aa","bb","cccc","dd"));
     }
+*/
 
-
-    }
+    }}
 
